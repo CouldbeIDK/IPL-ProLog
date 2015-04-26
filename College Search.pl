dd4f098bb53892d82ce8756e4ef62e40f08b0774
William Brownson <Williamiv95@gmail.com>
2015-04-25 23:47:44 -0700

    Hourly Commit
    Added a few students, and the first college, working on the syntax for
    arrays, and the methods able to be used on them

--- College Search.pl
+++ College Search.pl
@@ -1,3 +1,25 @@
+% STUDENTS
+
 % Sally Jenkins
-	student(sally_jenkins).
-	student_id(sally_jenkins, 1).
+
+	student_name(st01, sally_jenkins).
+	student_preferred(st01, co01).
+	student_gpa(st01, 3.25).
+
+% Jacob Salem
+
+	student_name(st02, jacob_salem).
+	student_preferred(st02, co01).
+	student_gpa(st02, 2.9).
+	
+% Aaron Bond
+
+	student_name(st03, aaron_bond).
+	student_preferred(st03, co02).
+	student_gpa(st03, 4.0).
+	
+% COLLEGES
+
+% Pacific Union College
+
+	college_name(co01, pacific_union_college).
