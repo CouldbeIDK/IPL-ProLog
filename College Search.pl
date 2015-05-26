f606ccea714559d2c36d2c5da7cbcd8d59ad0ce3
William Brownson <Williamiv95@gmail.com>
2015-05-26 03:09:44 -0700

    [5/26/2015 2:00 am - 3:00am]
    Work towards B-level, later I'm going to have the program suggest
    college the students are eligible for, if a preferred college is not
    available.

--- College Search.pl
+++ College Search.pl
@@ -1,36 +1,39 @@
 % STUDENTS
 
-% student(first, last, id, gpa, preferred college ID)
+% student(first & last, id, gpa, preferred college location, preferred college rating, preferred college size[1 = small, 2 = average size, 3 =large)
 
-	student('Sally Jenkins', st01, 3.25, co01).
+	student('Sally Jenkins', st01, 3.25, city, 7, 3).
 
-	student('James Saylor', st02, 2.9, co01).
+	student('James Saylor', st02, 2.9, isolated, 7, 2).
 
-	student('Aaron Bond', st03, 4.0, co02).
+	student('Aaron Bond', st03, 4.0, city, 9, 1).
 	
-	student('Anon Man', st04, 3.1, co01).
+	student('Anon Man', st04, 3.1, isolated,  6, 2).
 	
-	student('That Chick', st05, 1.0, co02).
+	student('That Chick', st05, 1.0, city, 4, 3).
 	
-	student('Some Dude', st06, 1.5, co01).
+	student('Some Dude', st06, 1.5, city, 5, 3).
 	
 % COLLEGES 
 
-% college(name, ID, MinGPA, is accepting students)
+% college(name, ID, MinGPA, is accepting students, location, ranking, size)
 
-	college('Pacific Union College', co01, 3.0, true).
+	college('Pacific Union College', co01, 3.0, true, isolated, 7.5, 2).
 	
-	college('Fresno Pacific Union', co02, 3.2, false).
+	college('Fresno Pacific Union', co02, 3.2, false, city, 6, 3).
 	
 % LOGIC
 
 	is_preferred(Student, College) :-
-		student(_, Student, _, Pref),
-		Pref == College.
+		student(_,Student,_,Ploc, Prat, Psiz),
+		college(_,College,_, Loc, Rat, Siz),
+		Ploc == Loc,
+		Prat <= Rat,
+		Siz - Psiz < 1 .
 		
 	is_eligible(Student, College) :-
-		student(_,Student,Gpa,_),
-		college(_,College, Min, true),
+		student(_,Student,Gpa,_,_,_),
+		college(_,College, Min, true,_,_,_),
 		Gpa >= Min.
 		
 	is_entered(Student, College) :-
